bf9b3cefd09b61cb85db1dc5de46067c550c2143
Hein Htat <heinthuhtat@gmail.com>
2026-01-19 11:55:37 -0600

    initial target for meshishi repeater

---
+++ variants/meshishi/pins_arduino.h
@@ -0,0 +1,20 @@
+#ifndef Pins_Arduino_h
+#define Pins_Arduino_h
+
+#include <stdint.h>
+
+#define USB_VID 0x303a
+#define USB_PID 0x1001
+
+// static const uint8_t TX = ...
+// static const uint8_t RX = ...
+
+static const uint8_t SS = 14;
+static const uint8_t SCK = 21;
+static const uint8_t MOSI = 38;
+static const uint8_t MISO = 39;
+
+static const uint8_t SCL = 18;
+static const uint8_t SDA = 8;
+
+#endif /* Pins_Arduino_h */
--- variants/meshishi/target.h
+++ variants/meshishi/target.h
@@ -7,8 +7,9 @@
 #include <helpers/radiolib/CustomSX1262Wrapper.h>
 #include <helpers/AutoDiscoverRTCClock.h>
 #include <helpers/SensorManager.h>
+#include "MeshishiBoard.h"
 
-extern ESP32Board board;
+extern MeshishiBoard board;
 extern WRAPPER_CLASS radio_driver;
 extern AutoDiscoverRTCClock rtc_clock;
 extern SensorManager sensors;
@@ -16,6 +17,5 @@ extern SensorManager sensors;
 bool radio_init();
 uint32_t radio_get_rng_seed();
 void radio_set_params(float freq, float bw, uint8_t sf, uint8_t cr);
-void radio_set_tx_power(int8_t dbm);
+void radio_set_tx_power(uint8_t dbm);
 mesh::LocalIdentity radio_new_identity();
-
